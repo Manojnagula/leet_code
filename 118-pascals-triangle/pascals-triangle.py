4eee1180a9ad2cafea6f0d5af20fc710e878d541
Manojnagula <122856265+Manojnagula@users.noreply.github.com>
2025-06-03 10:58:50 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (87.02%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -4,6 +4,6 @@ class Solution:
         for i in range(1,numRows+1):
             return_array.append([1]*i)
         for i in range(2,numRows):
-            for j in range(1,len(return_array[i])-1):
+            for j in range(1,i):
                 return_array[i][j] = return_array[i-1][j-1]+return_array[i-1][j]
         return return_array
